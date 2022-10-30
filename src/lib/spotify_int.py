c6d258030dec538130f636b0a2803439069fab5d
Paul Vivier <github@vivier.net>
2022-10-29 21:34:16 -0400

    Test script for raspberry pi

--- src/lib/spotify_int.py
+++
@@ -1,9 +0,0 @@
-from pydoc import cli
-from spotipy.oauth2 import SpotifyClientCredentials
-from spotipy.oauth2 import SpotifyOAuth
-import spotipy
-from pprint import pprint
-
-
-## import os
-## from time import sleep
